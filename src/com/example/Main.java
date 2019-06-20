cb44896445e12c6ba16725ffda60d65dde0b38e3
kristen <kristenmay0520@gmail.com>
2019-06-20 12:36:08 -0400

    first commit

---
+++ src/com/example/Main.java
@@ -0,0 +1,31 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        calcFeetAndInchesToCentimeters(20, 0);
+        calcFeetAndInchesToCentimeters(32);
+    }
+
+    public static int calcFeetAndInchesToCentimeters(int feet, int inches){
+        if(feet < 0 || inches > 12 || inches < 0){
+            return -1;
+        }
+        inches = inches + (feet * 12);
+        System.out.println("inches = " + inches);
+        double centimeters = inches * 2.54;
+        System.out.println((int) centimeters);
+        return (int) centimeters;
+    }
+
+    public static int calcFeetAndInchesToCentimeters(int inches){
+        if(inches < 0){
+            return -1;
+        }
+        int in = inches % 12;
+        int ft = (inches - in) / 12;
+        int cm = calcFeetAndInchesToCentimeters(ft, in);
+        return cm;
+    }
+
+}
